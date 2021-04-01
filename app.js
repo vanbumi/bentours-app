6bccb699bfd174ac4173ae8b2368818b19eff73e
vanbumi <jsp.dio@gmail.com>
2021-04-01 20:22:56 +0700

    Delete request

--- app.js
+++ app.js
@@ -87,6 +87,21 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   })
 })
 
+// DELETE REQUEST
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    })
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  })
+})
+
 
 // SETUP SERVER
 const PORT = process.env.PORT || 3000
