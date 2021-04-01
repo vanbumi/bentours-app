d9a065124a422be883d8fbaadb32d1e8180df910
vanbumi <jsp.dio@gmail.com>
2021-04-01 17:21:10 +0700

    Request method with Patch

--- app.js
+++ app.js
@@ -70,7 +70,25 @@ app.post('/api/v1/tours', (req, res) => {
   )
 })
 
+// PATCH REQUEST
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    })
+  }
 
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here....>'
+    }
+  })
+})
+
+
+// SETUP SERVER
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server berjalan di port ${PORT}...`)
